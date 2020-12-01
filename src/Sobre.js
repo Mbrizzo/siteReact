1f34bf0fb3c6c21ebe01e48231d03ac7d7c37c3d
MRizzo <mrizzo25@gmail.com>
2020-12-01 12:08:16 -0300

    video removed

--- src/Sobre.js
+++ src/Sobre.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import videoNatal from './assets/natal-redentor.mp4';
+
 import Head from './Head';
 
 const Sobre = () => {
   return (
     <>
       <Head title="Sobre" description="Essa é a página Sobre" />
-      <h2>A Fac Redentor, lhes deseseja um Feliz Natal!!!</h2>
-      <video src={videoNatal} autoPlay="false" controls="true"></video>
     </>
   );
 };
