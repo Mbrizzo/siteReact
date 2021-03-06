8e969c6e12c46c8106731472b6e4f396a2ddb342
Caio Machado <42222129+caiomachhado@users.noreply.github.com>
2021-03-06 20:43:26 -0300

    Change Texts

--- src/Testes.js
+++ src/Testes.js
@@ -12,10 +12,10 @@ export const Testes = () => {
         <Head title="Testes" description="Essa é a página de testes" />
       </div>
       <h4 className="h4">
-        Essa página será removida dia 27/11... ou talvez não... na verdade só
+        Essa página será removida dia 07/03... ou talvez não... na verdade só
         Deus sabe!!! Mas se nem eu me importo com isso, imagina você. Portanto
         não se preocupe mais com isso... apenas contemple, assombrado, essa
-        magnífica página de testes enquanto medita sobre sua vida ou os
+        magnífica página de testes enquanto medita sobre sua vida ou alguns
         mistérios do mundo ou quem sabe sobre seu cachorro mesmo que não tenha
         um.
       </h4>
